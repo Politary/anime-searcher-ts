425c181dc0a26c29a52099758e22d3ea2bf2dfec
Politary <sergey.2000.kambur@gmail.com>
2022-01-18 17:19:44 +0300

    routes are mapped now

--- src/App.js
+++ src/App.js
@@ -8,16 +8,51 @@ import { Layout } from './modules/layout/components/Layout/Layout.tsx';
 import { TitlePage } from './pages/TitlePage/TitlePage';
 import { SearchPage } from './pages/SearchPage/SearchPage';
 
+const routes = {
+    path: '/',
+    element: <Layout />,
+    children: [
+        {
+            index: true,
+            path: '',
+            element: <HomePage />,
+        },
+        {
+            index: false,
+            path: 'animes',
+            element: <AnimesPage />,
+        },
+        {
+            index: false,
+            path: 'search',
+            element: <SearchPage />,
+        },
+        {
+            index: false,
+            path: 'animes/:id/',
+            element: <TitlePage />,
+        },
+        {
+            index: false,
+            path: '*',
+            element: <NotFoundPage />,
+        },
+    ],
+};
+
 function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<HomePage />} />
-                    <Route path="animes" element={<AnimesPage />} />
-                    <Route path="search" element={<SearchPage />} />
-                    <Route path="animes/:id/" element={<TitlePage />} />
-                    <Route path="*" element={<NotFoundPage />} />
+                <Route path={routes.path} element={routes.element}>
+                    {routes.children.map(({ index, path, element }) => (
+                        <Route
+                            index={index}
+                            path={path}
+                            element={element}
+                            key={path}
+                        />
+                    ))}
                 </Route>
             </Routes>
         </>
