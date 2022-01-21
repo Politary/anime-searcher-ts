dc5a9a9cdcbdc8261a89cdec15b04c4880b9739d
Politary <sergey.2000.kambur@gmail.com>
2022-01-21 18:30:50 +0300

    removed unnecessary imports

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root.reducer';
